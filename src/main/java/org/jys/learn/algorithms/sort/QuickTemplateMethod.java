e94e340a151a5e5007ca8a6580cdfd369526ad93
jiangyuesong <1259012537@qq.com>
2019-06-28 18:37:03 +0800

    fixed quick sort problem

--- src/main/java/org/jys/learn/algorithms/sort/QuickTemplateMethod.java
+++ src/main/java/org/jys/learn/algorithms/sort/QuickTemplateMethod.java
@@ -7,7 +7,38 @@ package org.jys.learn.algorithms.sort;
  */
 public abstract class QuickTemplateMethod<T extends Comparable<T>> extends SortTemplateMethod<T> {
 
+
     public int  partition(T[] a, int lo, int hi){
+        T v=a[lo];
+        //第一次做，这里写为了i=lo
+        //这样，可能把边界lo交换了
+        //最后一步的交换lo和j就出错了
+        int i=lo+1;
+        int j=hi;
+        while (true){
+            while (less(a[i],v)){
+                i++;
+                if(i>=hi){
+                    break;
+                }
+            }
+            while (less(v,a[j])){
+                j--;
+                if(j<=lo){
+                    break;
+                }
+            }
+            if(i>=j){
+                break;
+            }
+            exchange(a,i,j);
+        }
+        exchange(a,lo,j);
+        return j;
+    }
+    //参照上面的做法吧，更容易理解，这个hi+1的做法不容易理解
+    //这个是书上的做法
+    public int  partitionReference(T[] a, int lo, int hi){
         T v=a[lo];
         int i=lo;
         int j=hi+1;
@@ -31,39 +62,6 @@ public abstract class QuickTemplateMethod<T extends Comparable<T>> extends SortT
         //如果i>j,并且a[i]>v,a[j]<v,就是...a[j]a[i]...,
         //交换i和lo，则交换后左侧的a[lo]>a[i]不满足切分要求
         //准确的说，是交换两个中的较小值，只是这个值一定是a[j]
-        exchange(a,lo,j);
-        return j;
-    }
-
-    /**
-     *下面这种方法是不对的，想了很久的原因，才发现
-     * 每次交换完毕后，被交换的数字又参与了一次笔记
-     * a[i] 和a[i]交换，下一次循环，又比较了，a[i]和a[j]，
-     * 这时候满足条件，i++了，j++了，这时候造成了多余的交换
-     * 临界下出错了
-     */
-    private int  partitionWrong(T[] a, int lo, int hi){
-        T v=a[lo];
-        int i=lo;
-        int j=hi;
-        while (true){
-            while (less(a[i],v)){
-                i++;
-                if(i>hi){
-                    break;
-                }
-            }
-            while (less(v,a[j])){
-                j--;
-                if(j<lo){
-                    break;
-                }
-            }
-            if(i>=j){
-                break;
-            }
-            exchange(a,i,j);
-        }
         exchange(a,lo,j);
         return j;
     }
